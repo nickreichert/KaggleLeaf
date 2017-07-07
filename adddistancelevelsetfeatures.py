2bbd6bd9ef40c2337706eb409a877358acd2b462
nickreichert <nwr@uw.edu>
2017-07-06 20:37:52 -0700

    Add files via upload
    Cleaned up file, added comments

--- adddistancelevelsetfeatures.py
+++ adddistancelevelsetfeatures.py
@@ -1,17 +1,32 @@
-import numpy as np                     # numeric python lib
+"""
+Created on Thu Apr 20 19:17:06 2017
 
-from PIL import Image
-import PIL.ImageOps
+@author: Nick
+
+Some of code/ideas inspired by Matthew McGonagle
+https://github.com/MatthewMcGonagle/KaggleLeaf/
+and by Lorinc Nyitrai
+https://www.kaggle.com/lorinc/feature-extraction-from-images
+
+This file computes various quantities associated with the distance from
+the edge of the leaf
+Specifically, it finds the maximum distance of a point in the leaf from the
+edge (stored in the variable "max"). Then it computes the proportion of the
+leaf which is a distance from the edge lying within the interval 
+[i, i + 1]*max/4 for 0 <= i <= 3 (these are the "hist" values) and saves
+the results to output. Also outputted is the ratio of max to the square root
+of the area of the leaf (the var "ratio"). "Ratio" can be considered as a
+sort of isoperimetric ratio.
+
+Output file results are scale, rotation, and reflection invariant
+"""
+
+import numpy as np                     
+
+import matplotlib.image as mpimg       
 import time
-import matplotlib.pyplot as plt
 
-
-import matplotlib.image as mpimg       # reading images to numpy arrays
-import matplotlib.pyplot as plt        # to plot any graph
-import matplotlib.patches as mpatches  # to draw a circle at the mean contour
-import time
-
-from skimage import measure, img_as_bool, io, color, morphology           # to find shape contour
+from skimage import measure, img_as_bool
 import scipy.ndimage as ndi            # to determine shape centrality
 
 numimg = 1584
@@ -21,8 +36,8 @@ lasttime = time.clock()
 for i in range(numimg):
     img = mpimg.imread('images/' + str(i + 1) + '.jpg')
     #img = ((img > 250) * 255).astype(img.dtype)   IF WORRIED ABOUT NOISE AT EDGE
-    dist_2d = ndi.distance_transform_edt(img)
-    total = np.count_nonzero(dist_2d)
+    dist_2d = ndi.distance_transform_edt(img)   #dist_2d contains the pointwise
+    total = np.count_nonzero(dist_2d)           #distances from the leaf edge
     max = np.amax(dist_2d)
     hist = np.histogram(dist_2d, bins = [0.5, max/4, max/2, 3*max/4, max + 0.5], range = (0.5, max + 1))[0]/total
     ratio = max/np.sqrt(total)
@@ -39,7 +54,7 @@ for i in range(numimg):
 print('Now Saving Results to file ', outputname)
 f = open(outputname, 'w')
 f.truncate()
-line = 'id,radius to sqrt(area) ratio,first quartile,second quartile,third quartile,fourth quartile\n'
+line = 'id,radius to sqrt(area) ratio,edge dist quartile 1,edge dist quartile 2,edge dist quartile 3,edge dist quartile 4\n'
 f.write(line)
 for i in range(numimg):
     line = str(geodata[i][0]) + ',' + str(geodata[i][1]) + ',' + str(geodata[i][2]) + ',' + str(geodata[i][3]) + ',' + str(geodata[i][4]) + ',' + str(geodata[i][5]) + '\n'
